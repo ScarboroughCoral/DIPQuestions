711aaa1684069650c2b08a0a51c1b00998342403
ScarboroughCoral <3249977074@qq.com>
2019-06-21 10:40:35 +0800

    finished Q1:swap the rgb channel

---
+++ channel_swapping.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread("imori.jpg")
+r=img[:,:,2].copy()
+g=img[:,:,1].copy()
+b=img[:,:,0].copy()
+
+img[:,:,0]=r
+img[:,:,1]=g
+img[:,:,2]=b
+
+cv2.imwrite("channel_swapping.jpg",img)
