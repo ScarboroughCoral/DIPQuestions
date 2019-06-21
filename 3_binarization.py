444301b95162c2a0e1f702e913a1510d39db1848
ScarboroughCoral <3249977074@qq.com>
2019-06-21 11:35:20 +0800

    finished Q4:binarization

---
+++ 3_binarization.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread("imori.jpg")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+th = 128
+
+gray[gray>=th]=255
+gray[gray<th]=0
+
+cv2.imwrite("3_binarization.jpg",gray)
